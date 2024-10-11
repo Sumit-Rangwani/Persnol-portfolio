bc6f9a28735e9d53cf13fe1ea9bf2b8fb65ec4ce
Sumit-Rangwani <maheshrangwani05@gmail.com>
2024-10-11 14:13:16 +0530

    code of persnol responce

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <>  
+      <Portfolio/>
+    </>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function Portfolio() {
+                
+    return (
+        <div>
+            <head>
+               <meta charSet="UTF-8"/>
+               <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+               <link 
+               rel="stylesheet"
+               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+               />
+          </head>
+
+    
+                        <div style={{ backgroundColor: 'white' }}>
+                            <header>
+                                <nav>
+                                    <div class='parent'>
+                                    
+                                    <h1>
+                                        HI,<br></br>
+                                        Myself <span>Sumit Rangwani.</span>
+                                  </h1>
+                                      <div>
+                                      I’m a first-year student at Thadomal Shahani Engineering College,<br></br> 
+                                      pursuing a degree in Electronics and Telecommunications. Currently,<br></br>
+                                      I’m diving into the world of programming and web development, focusing on <br></br>
+                                      C programming, JavaScript, and the fundamentals of creating dynamic websites<br></br>
+                                      As I explore new skills, I’m passionate about building a solid foundation in <br></br>
+                                      technology and discovering how I can contribute to innovative projects in the<br></br>
+                                      future.<br></br>
+                                      I believe that continuous learning is key to personal and professional growth, <br></br>
+                                      and I’m excited to embrace new challenges along the way.
+                                    </div>
+                                  
+                                
+                            
+                                    
+                                     
+                                      
+                                
+                                     </div>     
+                                
+                                </nav>
+                            </header>
+                        </div> 
+
+
+
+                    </div>
+                   
+                
+                    )
+                
+}
+
+ 
